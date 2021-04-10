058b7a8b824dfdd0b0cb05ea77e91a146d70b9aa
Nikita K <RamaPapa29@yandex.ru>
2021-04-11 00:51:06 +0300

    eslint fix

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,49 +1,49 @@
-import React, { useState, useEffect, Fragment } from "react";
-import { ILineGraphic } from "../../interfaces/graphics/ILineGraphic";
+import React, { useState, useEffect, Fragment } from 'react';
+import LineChartComponent from 'components/graphics/productHistoryPrice/ProductHistoryPriceGraphic';
+import { ILineGraphic } from '../../interfaces/graphics/ILineGraphic';
 import { getProduct } from '../../rest/product/RestProduct';
-import LineChartComponent from '../graphics/productHistoryPrice/ProductHistoryPriceGraphic';
 
-function Product(): JSX.Element {
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState(0);
-    const [graphicData, setGraphicData] = useState<ILineGraphic[]>([]);
+function Product() {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState(0);
+  const [graphicData, setGraphicData] = useState<ILineGraphic[]>([]);
 
-    useEffect(() => {
-        async function setProduct() {
-            const productData = await getProduct('Сок Я апельсин с мякотью 970 мл');
-            const { data } = productData || {};
-            const { name = '', price = 0 } = data[0] || {};
+  useEffect(() => {
+    async function setProduct() {
+      const productData = await getProduct('Сок Я апельсин с мякотью 970 мл');
+      const { data } = productData || {};
+      const { name = '', price = 0 } = data[0] || {};
 
-            const graphicData = data.map<ILineGraphic>((product) => {
-                const { date, price = 0 } = product;
+      const graphicData = data.map<ILineGraphic>((product) => {
+        const { date, price = 0 } = product;
 
-                return {
-                    name: date,
-                    value: price
-                }
-            })
-            setName(name);
-            setPrice(price);
-            setGraphicData(graphicData);
-        }
-        setProduct()
-    })
+        return {
+          name: date,
+          value: price,
+        };
+      });
+      setName(name);
+      setPrice(price);
+      setGraphicData(graphicData);
+    }
+    setProduct();
+  });
 
-    return (
-        <Fragment>
-            <div>
-                <span>Имя</span>
-                <span>{name}</span>
-            </div>
-            <div>
-                <span>Цена</span>
-                <span>{price}</span>
-            </div>
-            <div>
-                {LineChartComponent(graphicData)}
-            </div>
-        </Fragment>
-    );
+  return (
+    <>
+      <div>
+        <span>Имя</span>
+        <span>{name}</span>
+      </div>
+      <div>
+        <span>Цена</span>
+        <span>{price}</span>
+      </div>
+      <div>
+        {LineChartComponent(graphicData)}
+      </div>
+    </>
+  );
 }
 
 export default Product;
